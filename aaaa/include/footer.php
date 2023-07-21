d9e8ca2bf9f6f17147766239e41276fc3bb8f3e5
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-07-21 22:24:13 +0530

    footer section changes
    update

--- aaaa/include/footer.php
+++ aaaa/include/footer.php
@@ -3,8 +3,7 @@
     <div class="row justify-content-center align-items-center">
       <div class="col-xl-2 col-12 text-xl-left text-center  animate" data-animation="fadeInUp">
         <a href="index.php" class="logo">
-          <!-- <img src="img/logo.png" alt=""> -->
-          <h4 class="text-white">Che <span class="text-warning">s</span>s </h4>
+        <img class="img-fluid" src="img/logo02.png" alt="logo" width="150" height="150">
         </a>
       </div>
       <div class="col-xl-8 col-12 text-center animate" data-animation="fadeInUp">
@@ -39,23 +38,11 @@
     </div>
   </div>
 </footer>
-<section class="page_copyright copyright1 ds s-pt-70 s-pb-60 s-bordertop">
+<section class="page_copyright copyright1 ds s-pt-30 s-pb-30 s-bordertop">
   <div class="container">
     <div class="row align-items-center">
-      <div class="col-12">
-        <div class="widget widget_mailchimp">
-          <form class="signup" action="https://html.modernwebtemplates.com/">
-            <i class="far fa-envelope"></i>
-            <input name="email" type="email" class="form-control mailchimp_email" placeholder="Email address">
-            <button type="submit" class="search-submit">
-              <span class="screen-reader-text"> Subscribe </span>
-            </button>
-            <div class="response"></div>
-          </form>
-        </div>
-      </div>
       <div class="col-md-12 text-center">
-        <p>All Rights Reserved by <a href="index.php">Chess</a> 2023 | <span>Designed & Developed by <a href="https://shatbhuja.com/">shatbhuja</a>
+        <p>All Rights Reserved by <a href="index.php">UK Sports Federation</a> 2023 | <span>Designed & Developed by <a href="https://shatbhuja.com/">shatbhuja</a>
             <a class="d-none" href="https://infinitycoder.in">infinitycoder</a>
           </span>
         </p>
