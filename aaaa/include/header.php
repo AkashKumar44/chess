bbc73cf08caf54fd5c6213c7671bf46a56569d95
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-07-21 22:24:38 +0530

    header page section remove and update
    update

--- aaaa/include/header.php
+++ aaaa/include/header.php
@@ -157,19 +157,16 @@
           </div>
         </section>
         <div class="header_absolute ">
-          <!-- header with two Bootstrap columns - left for logo and right for navigation -->
           <header class="page_header ds ms bg-transparent justify-nav-center s-overlay s-py-10 " style="background-color: #fbfbfba6 !important;">
             <div class="container">
               <div class="row align-items-center">
                 <div class="col-xl-2 col-lg-3 col-md-7 col-8">
                   <a href="index.php" class="logo">
-                    <!-- <h4 class="text-white">Che <span class="text-warning">s</span>s </h4> -->
                     <img class="img-fluid" src="img/logo02.png" alt="logo">
                   </a>
                 </div>
                 <div class="col-xl-10 col-lg-9 col-md-5 col-1">
                   <div class="nav-wrap">
-                    <!-- main nav start -->
                     <nav class="top-nav justify-nav-center">
                       <ul class="nav sf-menu">
                         <li>
@@ -187,20 +184,8 @@
                         <li>
                           <a href="contact.php">Contact</a>
                         </li>
-                        <!-- <li><a href="#">Features</a><div class="mega-menu"><ul class="mega-menu-row"><li class="mega-menu-col"><a href="#">Headers</a><ul><li><a href="header1.html">Header Type 1</a></li><li><a href="header2.html">Header Type 2</a></li><li><a href="header3.html">Header Type 3</a></li><li><a href="header4.html">Header Type 4</a></li><li><a href="header5.html">Header Type 5</a></li><li><a href="header6.html">Header Type 6</a></li></ul></li><li class="mega-menu-col"><a href="#">Side Menus</a><ul><li><a href="header-side.html">Push Left</a></li><li><a href="header-side-right.html">Push Right</a></li><li><a href="header-side-slide.html">Slide Left</a></li><li><a href="header-side-slide-right.html">Slide Right</a></li><li><a href="header-side-sticked.html">Sticked Left</a></li><li><a href="header-side-sticked-right.html">Sticked Right</a></li></ul></li><li class="mega-menu-col"><a href="title1.html">Title Sections</a><ul><li><a href="title1.html">Title section 1</a></li><li><a href="title2.html">Title section 2</a></li><li><a href="title3.html">Title section 3</a></li><li><a href="title4.html">Title section 4</a></li><li><a href="title5.html">Title section 5</a></li><li><a href="title6.html">Title section 6</a></li></ul></li><li class="mega-menu-col"><a href="footer1.html#footer">Footers</a><ul><li><a href="footer1.html#footer">Footer Type 1</a></li><li><a href="footer2.html#footer">Footer Type 2</a></li><li><a href="footer3.html#footer">Footer Type 3</a></li><li><a href="footer4.html#footer">Footer Type 4</a></li><li><a href="footer5.html#footer">Footer Type 5</a></li><li><a href="footer6.html#footer">Footer Type 6</a></li></ul></li><li class="mega-menu-col"><a href="copyright1.html#copyright">Copyright</a><ul><li><a href="copyright1.html#copyright">Copyright 1</a></li><li><a href="copyright2.html#copyright">Copyright 2</a></li><li><a href="copyright3.html#copyright">Copyright 3</a></li><li><a href="copyright4.html#copyright">Copyright 4</a></li><li><a href="copyright5.html#copyright">Copyright 5</a></li><li><a href="copyright6.html#copyright">Copyright 6</a></li></ul></li></ul></div></li> -->
-                        <!-- eof features -->
-                        <!-- blog -->
-                        <!-- <li><a href="blog-right.html">Blog</a><ul><li><a href="blog-right.html">Right Sidebar</a></li><li><a href="blog-left.html">Left Sidebar</a></li><li><a href="blog-full.html">No Sidebar</a></li><li><a href="blog-single-right.html">Post</a><ul><li><a href="blog-single-right.html">Right Sidebar</a></li><li><a href="blog-single-left.html">Left Sidebar</a></li><li><a href="blog-single-full.html">No Sidebar</a></li></ul></li></ul></li> -->
-                        <!-- eof blog -->
-                        <!-- shop -->
-                        <!-- <li><a href="shop-right.html">Shop</a><ul><li><a href="shop-account-dashboard.html">Account</a><ul><li><a href="shop-account-details.html">Account details</a></li><li><a href="shop-account-addresses.html">Addresses</a></li><li><a href="shop-account-address-edit.html">Edit Address</a></li><li><a href="shop-account-orders.html">Orders</a></li><li><a href="shop-account-order-single.html">Single Order</a></li><li><a href="shop-account-downloads.html">Downloads</a></li><li><a href="shop-account-password-reset.html">Password Reset</a></li><li><a href="shop-account-login.html">Login/Logout</a></li></ul></li><li><a href="shop-right.html">Right Sidebar</a></li><li><a href="shop-left.html">Left Sidebar</a></li><li><a href="shop-product-right.html">Product Right Sidebar</a></li><li><a href="shop-product-left.html">Product Left Sidebar</a></li><li><a href="shop-cart.html">Cart</a></li><li><a href="shop-checkout.html">Checkout</a></li><li><a href="shop-order-received.html">Order Received</a></li></ul></li> -->
-                        <!-- eof shop -->
-                        <!-- contacts -->
-                        <!-- <li><a href="contact.html">Contact</a><ul><li><a href="contact.html">Contact 1</a></li><li><a href="contact2.html">Contact 2</a></li><li><a href="contact3.html">Contact 3</a></li><li><a href="contact4.html">Contact 4</a></li></ul></li> -->
-                        <!-- eof contacts -->
                       </ul>
                     </nav>
-                    <!-- eof main nav -->
                     <span class=" text-left text-md-right d-none d-xl-block">
                       <a href="#" class="btn btn-outline-maincolor">donate</a>
                     </span>
@@ -215,7 +200,6 @@
                           <i class="far fa-envelope"></i>
                         </a>
                       </li>
-                      <!-- <li><div class="dropdown"><a href="#" class="dropdown-toggle" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-search"></i></a><div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton2"><div class="widget widget_search"><form role="search" method="get" class="search-form" action="https://html.modernwebtemplates.com/"><label for="search-form-top"><span class="screen-reader-text">Search for:</span></label><input type="search" id="search-form-top" class="search-field" placeholder="Search keyword" value="" name="search"><button type="submit" class="search-submit"><span class="screen-reader-text">Search</span></button></form></div></div></div></li> -->
                     </ul>
                   </div>
                 </div>
