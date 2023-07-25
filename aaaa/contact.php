e63d0f42d1d68d3e150f01ae7cc8de416903fa26
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-07-25 23:05:42 +0530

    contact page update
    update

---
+++ aaaa/contact.php
@@ -0,0 +1,175 @@
+      <!-- header start -->
+      <?php include"include/header.php"?>
+
+      <section class="ls  s-py-xl-150 s-py-lg-130 s-py-md-90 s-py-60 c-gutter-60">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-lg-6 col-xl-7">
+							<p class="special-heading color-main sub-title">
+								<span class="text-uppercase">
+									contact us
+								</span>
+							</p>
+							<h2 class="special-heading">
+								<span class="text-capitalize ">
+									contact us
+								</span>
+							</h2>
+							<div class="fw-divider-space divider-30 divider-lg-60"></div>
+							<p>
+								Kids Chess Club is a non-profit Company founded by International Chess Master Larry Evans and Tom
+								Callaghan as a way to provide more chess classes to more kids.
+							</p>
+							<p>
+								Our goal is to teach children in grades K-12 abstract concepts of planning, analysis and strategy
+								through the game of chess. Kids will be excited to understand and amazed by the positive experience
+								that chess offers.
+							</p>
+							<div class="fw-divider-space divider-30 divider-lg-60"></div>
+							<div class="row c-gutter-30 c-mb-30">
+								<div class="col-xl-4 col-lg-6 col-md-4">
+									<div class="icon-box text-center bg-light small-padding box-shadow">
+										<div class="icon-styled color-main fs-52">
+											<i class="ico ico-smartphone"></i>
+										</div>
+										<p>
+											+1 234 056 78 90<br>
+											+1 234 056 78 90
+										</p>
+									</div>
+								</div>
+								<div class="col-xl-4 col-lg-6 col-md-4">
+									<div class="icon-box text-center bg-light small-padding box-shadow">
+										<div class="icon-styled color-main fs-52">
+											<i class="ico ico-placeholder"></i>
+										</div>
+
+										<p>
+											2688 Simpson <br> Avenue Harrisburg
+										</p>
+									</div>
+								</div>
+								<div class="col-xl-4 col-lg-6 col-md-4">
+									<div class="icon-box text-center bg-light small-padding box-shadow">
+										<div class="icon-styled color-main fs-52">
+											<i class="ico ico-email"></i>
+										</div>
+
+										<p>
+											<a href="https://html.modernwebtemplates.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="c3aaada5ac83a6bba2aeb3afa6eda0acae">[email&#160;protected]</a><br>
+											<a href="https://html.modernwebtemplates.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="4920272f26092c31282439252c672a2624">[email&#160;protected]</a>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-xl-5 animate" data-animation="scaleAppear">
+							<div class="form-wrapper px-lg-70 py-lg-100 px-20 py-60 cs ">
+								<form class="contact-form content-center c-mb-20 c-gutter-20" method="post" action="https://html.modernwebtemplates.com/">
+									<div class="row ">
+										<div class="col-12 form-title  text-center">
+											<h5>Contact Form</h5>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-12">
+											<div class="form-group has-placeholder">
+												<label for="name333">Your Name <span class="required">*</span></label>
+
+												<input type="text" aria-required="true" size="30" value="" name="name" id="name333" class="form-control" placeholder="Your Name">
+											</div>
+										</div>
+										<div class="col-sm-12">
+											<div class="form-group has-placeholder">
+												<label for="phone">Phone Number<span class="required">*</span></label>
+												<input type="tel" id="phone" class="form-control" pattern="^[0-9-+\s()]*$" name="phone" aria-required="true" placeholder="Phone Number">
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-12 ">
+											<div class="form-group has-placeholder">
+												<label for="message333">Message</label>
+												<textarea aria-required="true" rows="2" cols="45" name="message" id="message333" class="form-control" placeholder="Comment"></textarea>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-12 mt-lg-40 mt-15 text-center">
+											<div class="form-group">
+												<input class="btn btn-maincolor" type="submit" value="Contact us">
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+
+						</div>
+						<!--.col-* -->
+					</div>
+				</div>
+			</section>
+
+			<section class="container-px-0">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class=" ms page_map" data-draggable="false" data-scrollwheel="false">
+								<div class="marker">
+									<div class="marker-address">sydney, australia, Liverpool street, 66</div>
+									<div class="marker-title">Reach Us</div>
+									<div class="marker-description">
+
+										<ul class="list-unstyled">
+											<li>
+												<span class="icon-inline">
+													<span class="icon-styled color-main">
+														<i class="fa fa-map-marker"></i>
+													</span>
+
+													<span>
+														Sydney, Australia, Liverpool street, 66
+													</span>
+												</span>
+											</li>
+
+											<li>
+												<span class="icon-inline">
+													<span class="icon-styled color-main">
+														<i class="fa fa-phone"></i>
+													</span>
+
+													<span>
+														1 (800) 123-45-67
+													</span>
+												</span>
+											</li>
+											<li>
+												<span class="icon-inline">
+													<span class="icon-styled color-main">
+														<i class="fa fa-envelope"></i>
+													</span>
+
+													<span>
+														<a href="https://html.modernwebtemplates.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="8ce1ede5e0cce9f4ede1fce0e9a2efe3e1">[email&#160;protected]</a>
+													</span>
+												</span>
+											</li>
+										</ul>
+									</div>
+
+									<img class="marker-icon" src="images/map_marker_icon.png" alt="">
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+
+
+        
+        <!-- footer start -->
+      	<?php include"include/footer.php"?>
