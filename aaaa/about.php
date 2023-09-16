0d884bcb4ccd6b751e5ee96bb1dc6d63c434c352
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-09-16 23:40:16 +0530

    about page update
    update

--- aaaa/about.php
+++ aaaa/about.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+     <!DOCTYPE html>
      <head>
        <title>About</title>
        <meta charset="utf-8">
@@ -87,309 +87,6 @@
        </section>
        <!--Our Vision end-->
        
-       <!--counter start-->
-        <section>
-         <div class="container">
-           <div class="row">
-             <div class="divider-40 d-none d-lg-block"></div>
-             <div class="col-xl-3 col-lg-6 text-center">
-               <div class="chart" data-size="240" data-percent="75" data-line="4" data-bgcolor="#f5f5f5" data-trackcolor="#07a763" data-speed="3200">
-                 <div class="chart-meta">
-                   <strong class="percent color-darkgrey">75</strong>
-                   <h4 class="thin">web design</h4>
-                 </div>
-               </div>
-             </div>
-             <div class="col-xl-3 col-lg-6 text-center">
-               <div class="chart" data-size="240" data-percent="63" data-line="4" data-bgcolor="#f5f5f5" data-trackcolor="#ebc912" data-speed="4800">
-                 <div class="chart-meta">
-                   <strong class="percent color-darkgrey">63</strong>
-                   <h4 class="thin">mobile sites</h4>
-                 </div>
-               </div>
-             </div>
-             <div class="col-xl-3 col-lg-6 text-center">
-               <div class="chart" data-size="240" data-percent="88" data-line="4" data-bgcolor="#f5f5f5" data-trackcolor="#ebc912" data-speed="4100">
-                 <div class="chart-meta">
-                   <strong class="percent color-darkgrey">88</strong>
-                   <h4 class="thin">email design</h4>
-                 </div>
-               </div>
-             </div>
-             <div class="col-xl-3 col-lg-6 text-center">
-               <div class="chart" data-size="240" data-percent="84" data-line="4" data-bgcolor="#f5f5f5" data-trackcolor="#07a763" data-speed="3200">
-                 <div class="chart-meta">
-                   <strong class="percent color-darkgrey">84</strong>
-                   <h4 class="thin">user experience</h4>
-                 </div>
-               </div>
-             </div>
-             <div class="divider-40 d-none d-lg-block"></div>
-           </div>
-         </div>
-       </section>
-       <!-- counter end-->
-        
-        <!--team section start-->
-       <section class="ls s-py-md-90 s-py-60">
-				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<div class="row learn-press-courses isotope-wrapper masonry-layout c-gutter-30 c-mb-30" style="position: relative; height: 1320px;">
-								<div class="col-lg-4 col-md-6 col-12" style="position: absolute; left: 0%; top: 0px;">
-									<div class="vertical-item ls content-padding text-center course box-shadow">
-										<div class="item-media">
-											<a href="courses_single.html"><img src="images/courses/01.jpg" alt="img"></a>
-										</div>
-										<div class="item-content">
-											<h6>
-												<a href="courses_single.html">
-													Course: Garry Kasparov’s Masterclass
-												</a>
-											</h6>
-											<p>
-												Since the standard of play has significantly been raised
-											</p>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg-4 col-md-6 col-12" style="position: absolute; left: 33.3333%; top: 0px;">
-									<div class="vertical-item ls content-padding text-center course box-shadow">
-										<div class="item-media">
-											<a href="courses_single.html"><img src="images/courses/02.jpg" alt="img"></a>
-										</div>
-										<div class="item-content">
-											<h6>
-												<a href="courses_single.html">
-													Online Chess Lessons For Beginners
-												</a>
-											</h6>
-											<p>
-												Since the standard of play has significantly been raised
-											</p>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg-4 col-md-6 col-12" style="position: absolute; left: 66.6667%; top: 0px;">
-									<div class="vertical-item ls content-padding text-center course box-shadow">
-										<div class="item-media">
-											<a href="courses_single.html"><img src="images/courses/03.jpg" alt="img"></a>
-										</div>
-										<div class="item-content">
-											<h6>
-												<a href="courses_single.html">
-													International Chess School Online
-												</a>
-											</h6>
-											<p>
-												Since the standard of play has significantly been raised
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="mt--30"></div>
-						</div>
-					</div>
-				</div>
-			</section>
-			
-			
-			<section class="ls  s-py-md-90 s-py-60">
-				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<div class="row shortcode-service isotope-wrapper masonry-layout c-gutter-30 c-mb-30" style="position: relative; height: 647.594px;">
-								<div class="col-lg-4 col-md-6 col-12" style="position: absolute; left: 0%; top: 0px;">
-									<div class="vertical-item with-icon content-padding padding-small ls box-shadow text-center item-service">
-										<div class="item-media">
-											<a href="service-single.html">
-												<div class="icon-styled bg-maincolor round color-darkgrey fs-52">
-													<i class="ico ico-chess"></i>
-												</div>
-											</a>
-										</div>
-										<div class="item-content">
-											<h6><a href="service-single.html">The Players Of Chess</a></h6>
-											<p>
-												Our goal is to teach children in grades K-12 abstract
-											</p>
-											<div class="btn-service">
-												<a class="btn btn-darkgrey round" href="service-single.html">more</a>
-											</div>
-
-										</div>
-
-									</div>
-								</div>
-								<div class="col-lg-4 col-md-6 col-12" style="position: absolute; left: 33.3333%; top: 0px;">
-									<div class="vertical-item with-icon content-padding padding-small ls box-shadow text-center item-service">
-										<div class="item-media">
-											<a href="service-single.html">
-												<div class="icon-styled bg-maincolor round color-darkgrey fs-52">
-													<i class="ico ico-strategy"></i>
-												</div>
-											</a>
-										</div>
-										<div class="item-content">
-											<h6><a href="service-single.html">Top Chess Variants</a></h6>
-											<p>
-												Our goal is to teach children in grades K-12 abstract
-											</p>
-											<div class="btn-service">
-												<a class="btn btn-darkgrey round" href="service-single.html">more</a>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg-4 col-md-6 col-12" style="position: absolute; left: 66.6667%; top: 0px;">
-									<div class="vertical-item with-icon content-padding padding-small ls box-shadow text-center item-service">
-										<div class="item-media">
-											<a href="service-single.html">
-												<div class="icon-styled bg-maincolor round color-darkgrey fs-52">
-													<i class="ico ico-strategy-1"></i>
-												</div>
-											</a>
-										</div>
-										<div class="item-content">
-											<h6><a href="service-single.html">The Psyhology of Chess</a></h6>
-											<p>
-												Our goal is to teach children in grades K-12 abstract
-											</p>
-											<div class="btn-service">
-												<a class="btn btn-darkgrey round" href="service-single.html">more</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="mt--30"></div>
-						</div>
-					</div>
-				</div>
-			</section>
-			
-			
-			<section class="ls tax-fw-portfolio-category  s-py-md-90 s-py-60">
-				<div class="container">
-					<div class="row isotope-wrapper portfolio masonry-layout c-mb-30" data-filters=".gallery-filters" >
-						<div class="col-xl-4 col-sm-6 business news" >
-							<div class="vertical-item text-center content-padding box-shadow">
-								<div class="item-media">
-									<img src="images\courses/01.jpg" alt="#" height="450" width="400">
-									<div class="media-links">
-										<a class="abs-link" title="" href="gallery-single.html"></a>
-									</div>
-								</div>
-							</div>
-						</div>	
-						<div class="col-xl-8 col-sm-6 innovations corporate">
-							<div class="vertical-item text-center content-padding box-shadow">
-								<div class="item-content">
-
-									<h6>
-										<a href="gallery-single.html">Etiam tristique felis et convallis </a>
-									</h6>
-
-									<p>Donec lorem magna, dignissim nec hendrerit ut, laoreet ut nibh. Mauris pharetra, diam a interdum lacinia, nisl metus tincidunt.</p>
-                                    <p>Donec lorem magna, dignissim nec hendrerit ut, laoreet ut nibh. Mauris pharetra, diam a interdum lacinia, nisl metus tincidunt.</p>
-
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row isotope-wrapper portfolio masonry-layout c-mb-30" data-filters=".gallery-filters" >
-						<div class="col-xl-4 col-sm-6 business news" >
-							<div class="vertical-item text-center content-padding box-shadow">
-								<div class="item-media">
-									<img src="images\courses/01.jpg" alt="#" height="450" width="400">
-									<div class="media-links">
-										<a class="abs-link" title="" href="gallery-single.html"></a>
-									</div>
-								</div>
-							</div>
-						</div>	
-						<div class="col-xl-8 col-sm-6 innovations corporate">
-							<div class="vertical-item text-center content-padding box-shadow">
-								<div class="item-content">
-
-									<h6>
-										<a href="gallery-single.html">Etiam tristique felis et convallis </a>
-									</h6>
-
-									<p>Donec lorem magna, dignissim nec hendrerit ut, laoreet ut nibh. Mauris pharetra, diam a interdum lacinia, nisl metus tincidunt.</p>
-                                    <p>Donec lorem magna, dignissim nec hendrerit ut, laoreet ut nibh. Mauris pharetra, diam a interdum lacinia, nisl metus tincidunt.</p>
-
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row isotope-wrapper portfolio masonry-layout c-mb-30" data-filters=".gallery-filters" >
-						<div class="col-xl-4 col-sm-6 business news" >
-							<div class="vertical-item text-center content-padding box-shadow">
-								<div class="item-media">
-									<img src="images\courses/01.jpg" alt="#" height="450" width="400">
-									<div class="media-links">
-										<a class="abs-link" title="" href="gallery-single.html"></a>
-									</div>
-								</div>
-							</div>
-						</div>	
-						<div class="col-xl-8 col-sm-6 innovations corporate">
-							<div class="vertical-item text-center content-padding box-shadow">
-								<div class="item-content">
-
-									<h6>
-										<a href="gallery-single.html">Etiam tristique felis et convallis </a>
-									</h6>
-
-									<p>Donec lorem magna, dignissim nec hendrerit ut, laoreet ut nibh. Mauris pharetra, diam a interdum lacinia, nisl metus tincidunt.</p>
-                                    <p>Donec lorem magna, dignissim nec hendrerit ut, laoreet ut nibh. Mauris pharetra, diam a interdum lacinia, nisl metus tincidunt.</p>
-
-								</div>
-							</div>
-						</div>
-					</div>
-				
-				</div>
-			</section>
-       <!--team section end-->
-       
-        <!--Our Mission start-->
-        <style>
-            .our-mission {
-                    background-color: #f5f5f5;
-                    color: #5b5f66;
-                }
-        </style>
-        <section class="our-mission ls fw-team-template-default text-center text-sm-left single s-py-lg-130 s-py-md-90 s-py-60">
-         <div class="container">
-           <div class="row">
-             <div class="col-12">
-               <div class="side-item">
-                 <div class="item-media">
-                   <img src="images\courses/01.jpg" alt="team" width="400" height="500">
-                 </div>
-                 <div class="item-content">
-                   <p class="special-heading color-darkgrey  sub-title">
-                     <span class="text-uppercase"> Our Mission </span>
-                   </p>
-                   <h2 class="special-heading">
-                     <span class="text-capitalize "> Joseph A. Grasso </span>
-                   </h2>
-                   <div class="fw-divider-space divider-30 divider-lg-60"></div>
-                   <p> Kids Chess Club is a non-profit Company founded by International Chess Master Larry Evans and Tom Callaghan as a way to provide more chess classes to more kids. </p>
-                   <p> Our goal is to teach children in grades K-12 abstract concepts of planning, analysis and strategy through the game of chess. </p>
-                   <p>
-                     <img src="images/signature.png" alt="">
-                   </p>
-                 </div>
-               </div>
-             </div>
-           </div>
-         </div>
-       </section>
-       
         <section class=" s-pt-md-90 s-pt-60  s-pb-md-60 s-pb-30 overflow-visible c-gutter-30 c-mb-30">
          <div class="container ">
            <div class="row">
@@ -418,6 +115,99 @@
            </div>
          </div>
        </section>
+       
+       
+        <!--Our Mission start-->
+        <style>
+            .our-mission {
+                    background-color: #f5f5f5;
+                    color: #5b5f66;
+                }
+        </style>
+        <section class="our-mission ls fw-team-template-default text-center text-sm-left single  s-py-md-90 s-py-60">
+         <div class="container">
+           <div class="row">
+             <div class="col-12">
+               <div class="side-item">
+                 <div class="item-media">
+                   <img src="images\courses/01.jpg" alt="team" width="400" height="500">
+                 </div>
+                 <div class="item-content">
+                   <p class="special-heading color-darkgrey  sub-title">
+                     <span class="text-uppercase"> Our Mission </span>
+                   </p>
+                   <h2 class="special-heading">
+                     <span class="text-capitalize "> Joseph A. Grasso </span>
+                   </h2>
+                   <div class="fw-divider-space divider-30 divider-lg-60"></div>
+                   <p> Kids Chess Club is a non-profit Company founded by International Chess Master Larry Evans and Tom Callaghan as a way to provide more chess classes to more kids. </p>
+                   <p> Our goal is to teach children in grades K-12 abstract concepts of planning, analysis and strategy through the game of chess. </p>
+                   <p>
+                     <img src="images/signature.png" alt="">
+                   </p>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </section>
+       <section class="our-mission ls fw-team-template-default text-center text-sm-left single s-py-md-90 s-py-60">
+         <div class="container">
+           <div class="row">
+             <div class="col-12">
+               <div class="side-item">
+                
+                 <div class="item-content">
+                   <p class="special-heading color-darkgrey  sub-title">
+                     <span class="text-uppercase"> Our Mission </span>
+                   </p>
+                   <h2 class="special-heading">
+                     <span class="text-capitalize "> Joseph A. Grasso </span>
+                   </h2>
+                   <div class="fw-divider-space divider-30 divider-lg-60"></div>
+                   <p> Kids Chess Club is a non-profit Company founded by International Chess Master Larry Evans and Tom Callaghan as a way to provide more chess classes to more kids. </p>
+                   <p> Our goal is to teach children in grades K-12 abstract concepts of planning, analysis and strategy through the game of chess. </p>
+                   <p>
+                     <img src="images/signature.png" alt="">
+                   </p>
+                 </div>
+                  <div class="item-media">
+                   <img src="images\courses/01.jpg" alt="team" width="400" height="500">
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </section>
+       <section class="our-mission ls fw-team-template-default text-center text-sm-left single  s-py-md-90 s-py-60">
+         <div class="container">
+           <div class="row">
+             <div class="col-12">
+               <div class="side-item">
+                 <div class="item-media">
+                   <img src="images\courses/01.jpg" alt="team" width="400" height="500">
+                 </div>
+                 <div class="item-content">
+                   <p class="special-heading color-darkgrey  sub-title">
+                     <span class="text-uppercase"> Our Mission </span>
+                   </p>
+                   <h2 class="special-heading">
+                     <span class="text-capitalize "> Joseph A. Grasso </span>
+                   </h2>
+                   <div class="fw-divider-space divider-30 divider-lg-60"></div>
+                   <p> Kids Chess Club is a non-profit Company founded by International Chess Master Larry Evans and Tom Callaghan as a way to provide more chess classes to more kids. </p>
+                   <p> Our goal is to teach children in grades K-12 abstract concepts of planning, analysis and strategy through the game of chess. </p>
+                   <p>
+                     <img src="images/signature.png" alt="">
+                   </p>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </section>
+       
+       
         <!--Our Mission end-->
         
         
@@ -523,7 +313,6 @@
                      </span>
                    </div>
                  </div>
-                 
                  <div class="vertical-item cs item-team content-absolute content-show-hover text-center">
                    <div class="item-media">
                      <img src="images/team/rectangle/06.jpg" alt="team">
@@ -546,7 +335,6 @@
            </div>
          </div>
        </section>
-       
        <!--our team end-->
       
        <!-- footer start --> 
