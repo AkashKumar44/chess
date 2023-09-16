9282c80e144dc5fde0bfb1b2068704a95a705229
AkashKumar44 <54160276+AkashKumar44@users.noreply.github.com>
2023-09-16 23:40:44 +0530

    gallery page update
    update

---
+++ aaaa/gallery.php
@@ -0,0 +1,209 @@
+     <!DOCTYPE html>
+     <head>
+       <title> Uksportsfederation - Gallery</title>
+       <meta charset="utf-8">
+       <meta name="description" content="">
+       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+       <meta name="format-detection" content="telephone=no">
+       
+       <!-- header start --> 
+       <?php include"include/header.php"?> 
+       
+       <section class="page_title ds s-overlay s-pt-150 s-pb-50">
+				<div class="s-blur"></div>
+				<div class="container">
+					<div class="row">
+
+
+						<div class="col-md-12 text-center">
+							<h1>Gallery</h1>
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item">
+									<a href="index.php">Home</a>
+								</li>
+								<li class="breadcrumb-item">
+									<a href="#">Pages</a>
+								</li>
+								<li class="breadcrumb-item active">
+									Gallery
+								</li>
+							</ol>
+						</div>
+
+
+					</div>
+				</div>
+			</section>
+
+			<section class="ls portfolio s-py-xl-150 s-py-lg-120 s-py-md-80 s-py-50">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="row isotope-wrapper shortcode-gallery masonry-layout c-gutter-30 c-mb-30" data-filters=".gallery-filters">
+
+								<div class="col-xl-3 col-sm-6 business news">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/01.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-xl-3 col-sm-6 innovations corporate">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/02.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-xl-3 col-sm-6 entertainment news innovations">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/03.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-xl-3 col-sm-6 business entertainment">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/04.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-xl-3 col-sm-6 innovations corporate">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/05.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-xl-3 col-sm-6 entertainment news corporate">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/06.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-xl-3 col-sm-6 business news innovations">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/07.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-xl-3 col-sm-6 business entertainment corporate">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/08.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-xl-3 col-sm-6 business entertainment">
+									<div class="vertical-item item-gallery content-absolute content-show-hover text-center text-center cs">
+										<div class="item-media">
+											<img src="images/gallery/square/09.jpg" alt="img">
+										</div>
+										<div class="item-content ">
+											<h6>
+												<a href="#">Elitr sed diam nonumy</a>
+											</h6>
+											<div class="links-wrap">
+												<a class="link-zoom photoswipe-link" href="images/gallery/full/01.jpg"></a>
+												<a class="link-anchor" href="#"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- .isotope-wrapper-->
+						</div>
+					</div>
+				</div>
+			</section>
+			<div class="mt--30"></div>
+       
+       
+       
+       
+       
+       <!-- footer start --> 
+       <?php include"include/footer.php"?>
